f6ae3b57f3bd4ddd8d5a71e331606f1fd185f578
Daniel Marques <98705931+Marquezdan@users.noreply.github.com>
2023-03-30 21:54:28 -0300

    Adição dos comentários

--- arquivos_SQL/SQL_CREATE.sql
+++ arquivos_SQL/SQL_CREATE.sql
@@ -1,15 +1,18 @@
+-- Cria a tabela empresa com seus atributos e a chave primária.
 CREATE TABLE tb_empresa
 ( id_empresa INT not null,
  nome VARCHAR (50),
  endereco VARCHAR (50),
  PRIMARY KEY (id_empresa));
  
+ -- Cria a tabela empresa com seus atributos e a chave primária.
  CREATE TABLE tb_tecnologias
  ( id_tecnologias INT not null,
   nome VARCHAR (50),
   area VARCHAR (50),
   PRIMARY KEY (id_tecnologias));
   
+ -- Cria a tabela relatórios com seus atributos, chave primaria e estrangeira.
  CREATE TABLE tb_relatorios
  ( id_relatorios INT not null,
   id_empresa INT not null,
@@ -17,7 +20,7 @@ CREATE TABLE tb_empresa
   PRIMARY KEY(id_relatorios),
   FOREIGN KEY (id_empresa) REFERENCES tb_empresa(id_empresa));
  
- 
+-- Cria a tabela relacional dos relatórios e tecnologias com seus atributos, chave primarias e estrangeiras.
 CREATE TABLE relatorios_tecnologias (
     id_tecnologias INT NOT NULL,
     id_relatorios INT NOT NULL,
